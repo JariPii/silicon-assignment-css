105700fa6c4d9038b8141ec54a5caf57e804543a
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-09-08 17:51:51 +0200

    Adds darkmode starter
    Extremely basic darkmode toggle for now. Later I'll fix it to proper
    failsafe system checking fallback window match localstorage check.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-const burger = document.getElementById('burger');
+const burger = document.querySelector('#burger');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
 });
 
-const theme_switch = document.getElementById('switch');
+const theme_switch = document.querySelector('#switch');
+const html = document.documentElement;
 
 theme_switch.addEventListener('click', () => {
   theme_switch.classList.toggle('dark-mode');
+  html.classList.toggle('dark');
 });
